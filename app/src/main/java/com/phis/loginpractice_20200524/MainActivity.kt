e2f80a004636e9d154c1f17c56477590e18b84e3
parkcrystal <9jeworld@naver.com>
2020-05-24 11:26:12 +0900

    아이디 / 패스워드 입력

--- app/src/main/java/com/phis/loginpractice_20200524/MainActivity.kt
+++ app/src/main/java/com/phis/loginpractice_20200524/MainActivity.kt
@@ -2,11 +2,30 @@ package com.phis.loginpractice_20200524
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+
+            /*
+            var inputId:String = "테스트"
+            val inputPw:String = "상수테스트"
+            
+            inputId = "조경진" 
+            inputPw = "상수값 변경불가" */
+
+            val inputId = idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+
+            Log.d("입력아이디", inputId)
+            Log.d("입력비번", inputPw)
+
+        }
     }
 }
